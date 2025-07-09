e6fede50b6eac32c13234737bf4a9d4a4e911eac
rafika khatun <rafikakhatun607@gmail.com>
2025-07-09 21:05:30 +0530

    Setup React Router and added Link tags to Navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,20 +1,24 @@
-function Navbar(){
+import { Link } from 'react-router-dom';
 
-
-return(
-
-    
+function Navbar() {
+  return (
     <>
+      <section>
+        <nav>
+          <img className="w-26 h-14" src="src/assets/apna logo.png" alt="" />
 
-    <h1>hello</h1>
-    
-    
+          <ul>
+            <li><Link to="/results">Our Results</Link></li>
+            <li><Link to="/sigma">New Sigma 8.0</Link></li>
+            <li><Link to="/dsa-sheet">DSA Sheet</Link></li>
+            <li><Link to="/new-course">New Course</Link></li>
+            <li><Link to="/login">Log in</Link></li>
+            <li><Link to="/signup">Sign up</Link></li>
+          </ul>
+        </nav>
+      </section>
     </>
-)
-
-
-
-
+  );
 }
 
 export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+const route = createBrowserRouter([
+
+{
+  path:"/",
+  element:<App></App>
+}
+
+
+])
+
+
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+
+    <RouterProvider router={route}/>
+    
+   
   </StrictMode>,
 )
