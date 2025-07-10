7926f7864cad3ad470e0180c2051281aa7599790
rafika khatun <rafikakhatun607@gmail.com>
2025-07-10 19:28:06 +0530

    Install lucide-react and add icons in Navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -7,13 +7,26 @@ function Navbar() {
         <nav className='flex justify-between items-center px-6'>
           <img className="w-26 h-14" src="src/assets/apna logo.png" alt="" />
 
-          <ul className='flex gap-6 text-lg  '>
+          {/* Desktop Menu */}
+          <ul className='md:flex lg:flex hidden gap-6 text-lg  '>
             <li><Link to="/results">Our Results</Link></li>
             <li className='bg-[#6674cc] hover:bg-indigo-500 text-white py-2 px-6 rounded-md '><Link to="/sigma">New Sigma 8.0</Link></li>
             <li><Link to="/dsa-sheet">DSA Sheet</Link></li>
             <li><Link to="/new-course">New Course</Link></li>
             <li><Link to="/login">Log in</Link></li>
             <li className='px-3 py-1 border-2 border-[#6674cc] text-[#6674cc] rounded-md'><Link to="/signup">Sign up</Link></li>
+          </ul>
+
+          <ul className='flex md:hidden lg:hidden'>
+            
+              <li className=''>Our Result</li>
+              <li className=''>New Sigma 8.0</li>
+              <li className=''>DSA Sheet</li>
+              <li className=''>New Courses</li>
+              <li className=''>Log in</li>
+              <li className=''>Sign up</li>
+          
+
           </ul>
         </nav>
       </section>
