867d608b0a43d708825b99252726cb72f4f1758b
rafika khatun <rafikakhatun607@gmail.com>
2025-07-12 20:27:58 +0530

    Added Helpful Links section to footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,19 +4,28 @@ function Footer() {
     return (
         <>
 
-            <section className="w-full py-20 px-22">
-                <footer>
+            <section className="w-full py-20 px-">
+                <footer className="flex flex-col md:flex-row justify-between items-center">
 
                     {/* profile information link */}
-                    <div className="flex flex-col space-y-2">
+                    <div className="flex flex-col space-y-4">
                         <img src={apnaCollegeLogo} alt="Logo" width="100" height="100" />
                         <a className="text-[#6674CC] text-md md:text-lg" href="#">
-                           Where education meets <br className="hidden md:block" /> real-world needs.
+                            Where education meets <br className="hidden md:block" /> real-world needs.
                         </a>
                     </div>
 
+                    {/* helpful links */}
+                    <div className="flex flex-col">
+                        <p className="text-[#6674CC] font-bold">HELPFUL LINKS</p>
+                        <a className="text-[#6674CC] mt-2" href="#">Courses</a>
+                        <a className="text-[#6674CC] mt-2" href="#">Privacy Policy</a>
+                        <a className="text-[#6674CC] mt-2" href="#">Refund Policy</a>
+                        <a className="text-[#6674CC] mt-2" href="#">Terms & Conditions</a>
+                    </div>
 
-                    <div></div>
+
+
                     <div></div>
                     <div></div>
 
