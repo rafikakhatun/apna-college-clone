833887ed4241adbebfd3d371a3711b311d096090
rafika khatun <rafikakhatun607@gmail.com>
2025-07-16 21:18:09 +0530

    Started styling Login modal using Tailwind CSS

--- src/modals/LoginModal.jsx
+++ src/modals/LoginModal.jsx
@@ -1,14 +1,26 @@
-function LoginModal({isOpenLoginModal,onCloseLoginModal}){
-    if(!isOpenLoginModal){
+function LoginModal({ isOpenLoginModal, onCloseLoginModal }) {
+    if (!isOpenLoginModal) {
         return false;
     }
 
-    return(
+    return (
         <>
-        <div className="fixed z-50 bg-black">hello i am modal</div>
+            {/* overlay */}
+            <div className="fixed z-50 bg-black">
+                {/*modal box*/}
+                <div>
+                    {/* close button */}
+                    <button onClick={onClose} className="absolute top-4 right-4 text-neutral-500 hover:text-neutral-400 text-xl font-bold">
+                        <X size={24} />
+                    </button>
 
-        
-        
+
+
+                </div>
+            </div>
+
+
+
         </>
     )
 }
