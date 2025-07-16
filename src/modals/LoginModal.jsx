9ef91f1286adc3a011225f648f0dcb176eae44e5
rafika khatun <rafikakhatun607@gmail.com>
2025-07-16 21:31:24 +0530

    Added input fields for email and password

--- src/modals/LoginModal.jsx
+++ src/modals/LoginModal.jsx
@@ -1,3 +1,5 @@
+import { X } from "lucide-react";
+
 function LoginModal({ isOpenLoginModal, onCloseLoginModal }) {
     if (!isOpenLoginModal) {
         return false;
@@ -6,13 +8,30 @@ function LoginModal({ isOpenLoginModal, onCloseLoginModal }) {
     return (
         <>
             {/* overlay */}
-            <div className="fixed z-50 bg-black">
+            <div className="fixed bg-black/80 inset-0 bg-opacity-70 flex justify-center items-center z-50">
                 {/*modal box*/}
-                <div>
+                <div className="p-8 shadow w-[90%] max-w-md bg-black text-white relative">
                     {/* close button */}
-                    <button onClick={onClose} className="absolute top-4 right-4 text-neutral-500 hover:text-neutral-400 text-xl font-bold">
+                    <button onClick={onCloseLoginModal} className="absolute top-4 right-4 text-neutral-500 hover:text-neutral-400 text-xl font-bold">
                         <X size={24} />
                     </button>
+                    {/*heading*/}
+                    <h1 className="text-md font-semibold text-center mb-8">LOGIN OR SIGN UP TO START LEARNING</h1>
+
+                    {/*login form input field*/}
+                    <form className="space-y-6">
+                        <div>
+                            <label className="block mb-2 text-sm"  htmlFor="">What's your e-mail</label>
+                            <input type="text" placeholder="Name" className="w-full px-4 py-2 bg-neutral-800  outline-none  placeholder:text-neutral-500 placeholder:text-xs" />
+                        </div>
+                        <div>
+                            <label className="block mb-2 text-sm" htmlFor="">Your password</label>
+                            <input type="password"  placeholder="Password" className="w-full px-4 py-2 bg-neutral-800  outline-none  placeholder:text-neutral-500 placeholder:text-xs" />
+                        </div>
+
+                    </form>
+
+                    
 
 
 
