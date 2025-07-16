8b091add302e78efe75a645f1975e72b1874fead
rafika khatun <rafikakhatun607@gmail.com>
2025-07-16 21:51:50 +0530

    Enabled responsive login modal open/close functionality

--- src/modals/LoginModal.jsx
+++ src/modals/LoginModal.jsx
@@ -12,26 +12,26 @@ function LoginModal({ isOpenLoginModal, onCloseLoginModal }) {
                 {/*modal box*/}
                 <div className="p-8 shadow w-[80%] max-w-md bg-black text-white relative">
                     {/* close button */}
-                    <button onClick={onCloseLoginModal} className="absolute top-4 right-4 text-neutral-500 hover:text-neutral-400 text-xl font-bold">
-                        <X size={24} />
+                    <button onClick={onCloseLoginModal} className="absolute top-3 right-2  text-neutral-500 hover:text-neutral-400 text-xl font-bold">
+                        <X size={20} />
                     </button>
                     {/*heading*/}
-                    <h1 className="text-md font-semibold text-center mb-8">LOGIN OR SIGN UP TO START LEARNING</h1>
+                    <h1 className="text-md font-semibold text-center mb-6">LOGIN OR SIGN UP TO START LEARNING</h1>
 
                     {/*login form input field*/}
-                    <form className="space-y-6">
+                    <form className="space-y-5">
                         <div>
-                            <label className="block mb-2 text-sm"  htmlFor="">What's your e-mail</label>
-                            <input type="text" placeholder="Name" className="w-full px-4 py-2 bg-neutral-800  outline-none  placeholder:text-neutral-500 placeholder:text-xs" />
+                            <label className="block mb-1 text-sm"  htmlFor="">What's your e-mail</label>
+                            <input type="email" placeholder="Email" className="w-full px-4 py-2 bg-neutral-800  outline-none  placeholder:text-neutral-500 placeholder:text-xs" />
                         </div>
                         <div>
-                            <label className="block mb-2 text-sm" htmlFor="">Your password</label>
-                            <input type="password"  placeholder="Password" className="w-full px-4 py-2 bg-neutral-800  outline-none  placeholder:text-neutral-500 placeholder:text-xs" />
+                            <label className="block mb-1 text-sm" htmlFor="">Your password</label>
+                            <input type="password"  placeholder="Password" className="w-full  px-4 py-2 bg-neutral-800  outline-none  placeholder:text-neutral-500 placeholder:text-xs" />
                         </div>
 
                         {/* login button */}
 
-                        <button type="submit" className="w-full bg-[#6674cc] hover:bg-indigo-600 text-white py-2 font-semibold">
+                        <button type="submit" className="w-full text-sm bg-[#6674cc] hover:bg-indigo-600 text-white py-2 font-semibold">
                             Login
                         </button>
 
