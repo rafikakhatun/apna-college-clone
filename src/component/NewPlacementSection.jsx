1796dea8d555affa5ee71fb62f2acbbe65d95436
rafika khatun <rafikakhatun607@gmail.com>
2025-08-22 22:18:22 +0530

    feat: add AOS animations to NewPlacementSection

--- src/component/NewPlacementSection.jsx
+++ src/component/NewPlacementSection.jsx
@@ -4,28 +4,21 @@ function NewPlacementSection() {
 
     return (
         <>
-            <section className="py-12 px-4 flex justify-center items-center ">
-                <div className="text-center space-y-4">
-                    <h1 className="text-[#354Ac5] ">New PLACEMENT PREP Batch🔥
+            <section className="py-12 px-4 flex justify-center items-center " data-aos="fade-up">
+                <div className="text-center space-y-4" data-aos="fade-up">
+                    <h1 className="text-[#354Ac5]" data-aos="zoom-in">New PLACEMENT PREP Batch🔥
                     </h1>
-                    <p className="text-xl font-semibold">Sigma 8.0 : Development + DSA + Aptitude</p>
-                    <p className="text-gray-800">Start your placement preparation today!</p>
-                    <div className="flex justify-center items-center">
-                        <button className="flex   bg-[#6674cc] px-6 py-3 rounded-md text-white font-semibold">Explore more
-                            <ChevronsRight size={20} className="mt-1"  />
+                    <p className="text-xl font-semibold" data-aos="fade-up">Sigma 8.0 : Development + DSA + Aptitude</p>
+                    <p className="text-gray-800" data-aos="fade-up">Start your placement preparation today!</p>
+                    <div className="flex justify-center items-center" data-aos="zoom-in">
+                        <button className="flex bg-[#6674cc] px-6 py-3 rounded-md text-white font-semibold" data-aos="zoom-in">Explore more
+                            <ChevronsRight size={20} className="mt-1" />
                         </button>
-
                     </div>
                 </div>
             </section>
-
-
-
         </>
-
     )
-
-
 }
 
 export default NewPlacementSection;
