6e03afdca0f26a96b008fb13d5a690d3a5649283
rafika khatun <rafikakhatun607@gmail.com>
2025-10-08 22:25:09 +0530

    Add DsaSheet page content with heading and note

--- src/navbarPage/DsaSheet.jsx
+++ src/navbarPage/DsaSheet.jsx
@@ -6,6 +6,12 @@ const DsaSheet = () => {
     return (
        <>
        <Navbar></Navbar>
+       <section className='px-10 py-14 flex flex-col justify-center items-center'>
+
+        <h1 className='text-xl  font-semibold mb-2'><span className='text-[#6674cc]'>DSA PRACTICE</span> SHEET</h1>
+        <p className='text-gray-700 text-sm'>Note - This Sheet is only meant for <b>Alpha</b> & <b>Sigma </b>students.</p>
+
+       </section>
        <Footer></Footer>
        
        
